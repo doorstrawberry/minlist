4e653b0fb07a18a63ae6a2378c3312c3e9b8b394
doorstrawberry <kangminsoo671@gmail.com>
2022-06-10 13:51:14 -0400

    test route is functioning properly

---
+++ server.js
@@ -0,0 +1,23 @@
+// importing dependencies
+require("dotenv").config() // need this to access the PORT variable in the .env file
+const express = require("express") // need this to use express
+const methodOverride = require("method-override") // need this to be able to make update and delete routes
+const path = require("path") // need this in order to make referring to files easier
+const mongoose = require("./models/connections.js") // need this to establish connection with our database and to use mongoose
+
+// creating our application object
+const app = require("liquid-express-views")(express(), { root: [path.resolve(__dirname, 'views/')] }) // also using liquid epxress views
+
+// middleware
+app.use(methodOverride("_method")) // to use the method-override dependency
+app.use(express.urlencoded({extended: true})) // to be able to log data sent throughout the website
+
+// routes
+// testing route (will soon later become a log in)
+app.get("/", (req, res) => {
+    res.send("Checking route")
+})
+
+// server listener
+const PORT = process.env.PORT // accessing the port from the env file
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`)) // provides feedback on which port the application is using
