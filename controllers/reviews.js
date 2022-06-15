21fdd64145de5de493e1fda4b079d5f9afd38034
doorstrawberry <kangminsoo671@gmail.com>
2022-06-15 09:48:15 -0400

    revies will have id from and id to keys

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -24,6 +24,8 @@ router.post("/:toid/new/:fromid", (req, res) => {
         .then((fromUser) => {
             Users.findById(req.params.fromid, function (err, forUser) {
                 req.body.username = fromUser.name
+                req.body.writtenBy = req.params.fromid
+                req.body.reviewFor = req.params.toid
                 forUser.reviews.push(req.body)
                 forUser.save(function (err) {
                     res.redirect(`/users/${req.params.fromid}/accountview/${req.params.toid}`)
